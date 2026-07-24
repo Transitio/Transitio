cdbb026577059fa54f4345217ae2d8bb5805f215
Hitesh Kumar <hstarkar87@gmail.com>
2026-07-24 13:11:24 +0530

    Add nullable string variable in AssertionExtensions

--- src/Transitio.Assertions/AssertionExtensions.cs
+++ src/Transitio.Assertions/AssertionExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Transitio.Assertions;
 
@@ -20,6 +21,8 @@ namespace Transitio.Assertions;
 /// </summary>
 public static class AssertionExtensions
 {
+    string? maybechange = null;
+    string mustnotnull = maybechange;
     /// <summary>Begins an assertion chain for any object or value (equality, nullability, type).</summary>
     /// <remarks>
     /// This fallback is intentionally non-generic. A generic <c>Should&lt;T&gt;(this T)</c> would win
